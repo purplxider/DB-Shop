71e233255d6d7a5a37e8586ecaee11ad07b34dfd
josephy93 <38218466+josephy93@users.noreply.github.com>
2019-11-26 14:20:35 +0900

    Add files via upload

---
+++ User_info.sql
@@ -0,0 +1,21 @@
+insert into User_Info values ('0000001', '김진아', '2468kjazz', "1992-05-23", "F", "Silver", "경상북도 구미시", "도량동 그린빌 아파트 204동 1502호");
+insert into User_Info values ('0000002', '이주성', '2345ljshaha', "1991-04-21", "M", "Gold", "서울특별시 동작구", "상도동 래미안아파트 105동 1201호");
+insert into User_Info values ('0000003', '임준하', '874ljhbb', "1988-03-15", "M", "VIP", "서울특별시 강남구", "삼성동 중앙하이츠 빌리지 301동 203호");
+insert into User_Info values ('0000004', '이정민', '394ljmzizi', "1995-05-123", "F", "VVIP", "충청북도 제천시", "모산동 로즈웰힐아파트 201동 507호");
+insert into User_Info values ('0000005', '정현아', '429jharere', "1997-12-24", "F", "Silver",  "대전광역시 대덕구", "대화동 두레아파트 102동 511호");
+insert into User_Info values ('0000006', '임정아', '953lzadindin', "2000-01-04", "F", "Silver",  "대전광역시 동구", "신흥동 주공아파트 402동 602호");
+insert into User_Info values ('0000007', '조현주', '928jhjbalbal', "1994-06-03", "F", "Gold", "경기도 과천시", "중앙동 에코팰리스 아파트 501동 1102호");
+insert into User_Info values ('0000008', '박지선', '492pjskoko', "1999-03-29", "F", "Gold", "부산광역시 기장군", "기장읍 이진캐스빌블루 101동 406호");
+insert into User_Info values ('0000009', '이재민', '391ljmbakbak', "1989-11-05", "M", "VIP", "경상남도 김해시", "부영동 부영e-그린아파트 401동 1301호");
+insert into User_Info values ('0000010', '오병환', '195obhkeekee', "1986-08-12", "M", "VIP", "강원도 원주시", "단계동 단계현진 504동 608호");
+insert into User_Info values ('0000011', '최승훈', '129cshbanban', "1999-02-22", "M", "VVIP", "경상북도 포항시", "지곡동 제철아프트 203동 1103호");
+insert into User_Info values ('0000012', '신현주', '182shjseisei', "2001-08-04", "M", "Silver", "경기도 수원시", "팔달구 매산로1가 아이메카빌딩 201 - 302");
+insert into User_Info values ('0000013', '이규상', '018lklgalgal', "1995-04-28", "M", "Gold", "부산광역시", "반여동 1316번지");
+insert into User_Info values ('0000014', '박영희', '928pyhweiwei', "1991-05-30", "F", "Gold", "대전광역시", "유성구 대학로 291");
+insert into User_Info values ('0000015', '함원준', '838hjwhah', "1993-09-15", "M", "VIP", "충청북도 청주시", "서원구 남사로 7번길 14-2");
+insert into User_Info values ('0000016', '전상우', '958jskbaba', "1995-09-14", "M", "VVIP", "세종특별시", "한누리대로 어진동 499");
+insert into User_Info values ('0000017', '송나은', '825snugaga', "1996-07-30", "F", "Silver", "대구광역시", "서구 평리5동 국채보상로43길");
+insert into User_Info values ('0000018', '김희성', '952khsbubu', "1998-04-15", "M", "Silver", "광주광역시", "광산구 목련로273번길 76");
+insert into User_Info values ('0000019', '배민수', '359bnstata', "1995-08-16", "M", "Silver", "경기도 고양시", "일산서구 일산로612");
+insert into User_Info values ('0000020', '배민수', '853seklele', "2000-02-02", "F", "Gold", "경기도 파주시", "파평면 두포리 121");
+
